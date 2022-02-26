92044ef1733e5b8b6195d24fdc77ee52c6cda265
Nicolas P <niparrara@unal.edu.co>
2022-02-26 16:25:05 -0500

    format with prettier

--- src/utils.ts
+++ src/utils.ts
@@ -1,28 +1,35 @@
-
 function isString(value: any) {
-    return typeof (value) === 'string';
+  return typeof value === "string";
 }
 
-export const replaceLastOccurrenceInString = (input: any, find: any, replaceWith: any) => {
-    if (!isString(input) || !isString(find) || !isString(replaceWith)) {
-        // returns input on invalid arguments
-        return input;
-    }
+export const replaceLastOccurrenceInString = (
+  input: string,
+  find: string,
+  replaceWith: string
+) => {
+  if (!isString(input) || !isString(find) || !isString(replaceWith)) {
+    // returns input on invalid arguments
+    return input;
+  }
 
-    const lastIndex = input.lastIndexOf(find);
-    if (lastIndex < 0) {
-        return input;
-    }
+  const lastIndex = input.lastIndexOf(find);
+  if (lastIndex < 0) {
+    return input;
+  }
 
-    return input.substr(0, lastIndex) + replaceWith + input.substr(lastIndex + find.length);
-}
+  return (
+    input.substr(0, lastIndex) +
+    replaceWith +
+    input.substr(lastIndex + find.length)
+  );
+};
 
 export const capitalice = (value: string) => {
-    return value[0].toUpperCase() + value.substring(1, value.length)
-}
+  return value[0].toUpperCase() + value.substring(1, value.length);
+};
 
 export const template = (nameFile: string) =>
-    `import { makeStyles, Theme } from "@material-ui/core";
+  `import { makeStyles, Theme } from "@material-ui/core";
 
 const use${nameFile}s = makeStyles((theme: Theme) => ({
 
@@ -30,4 +37,4 @@ const use${nameFile}s = makeStyles((theme: Theme) => ({
 
 export default use${nameFile}s;
 
-`
+`;
